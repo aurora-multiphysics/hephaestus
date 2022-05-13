e8531ffc4aa2055f89d17b10e399e69386fab9a0
alexanderianblair <alexander.blair@ukaea.uk>
2022-05-13 15:01:17 +0000

    Formatting and CMake fixes

--- test/regression/Main.cpp
+++ test/regression/Main.cpp
@@ -1,8 +1,13 @@
+#include "mfem.hpp"
 #include <gtest/gtest.h>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   std::cout << "Regression tests from test/regression/Main.cpp" << std::endl;
+  int result = 0;
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  MPI_Init(&argc, &argv);
+  result = RUN_ALL_TESTS();
+  MPI_Finalize();
+  return result;
 }
