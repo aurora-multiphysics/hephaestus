3f96ea554ef0da7948b184acc1d7944838079cf7
alexanderianblair <alexander.i.blair@gmail.com>
2022-07-08 00:20:34 +0000

    Move coefficient and vector initialisation into hcurl_solver::Init

--- src/hephaestus_lib/e_solver.cpp
+++ src/hephaestus_lib/e_solver.cpp
@@ -19,10 +19,23 @@ void ESolver::SetVariableNames() {
 
 void ESolver::SetMaterialCoefficients(
     hephaestus::DomainProperties &domain_properties) {
-  sigmaCoef = domain_properties.scalar_property_map["electrical_conductivity"];
-  muCoef = domain_properties.scalar_property_map["magnetic_permeability"];
-  muInvCoef = new mfem::TransformedCoefficient(&oneCoef, muCoef, fracFunc);
-  dtMuInvCoef = new mfem::TransformedCoefficient(&dtCoef, muInvCoef, prodFunc);
+  alphaCoef = new mfem::TransformedCoefficient(
+      &oneCoef, domain_properties.scalar_property_map["magnetic_permeability"],
+      fracFunc);
+  betaCoef = domain_properties.scalar_property_map["electrical_conductivity"];
+}
+
+void ESolver::WriteConsoleSummary(double t, int it) {
+  // Write a summary of the timestep to console.
+
+  // Output Ohmic losses to console
+  double el = this->ElectricLosses();
+  if (myid_ == 0) {
+    std::cout << std::fixed;
+    std::cout << "step " << std::setw(6) << it << ",\tt = " << std::setw(6)
+              << std::setprecision(3) << t
+              << ",\tdot(E, J) = " << std::setprecision(8) << el << std::endl;
+  }
 }
 
 double ESolver::ElectricLosses() const {
