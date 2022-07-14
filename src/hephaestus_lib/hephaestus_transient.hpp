8b983c3694d22da10eb8565156a6a7a0553088ab
alexanderianblair <alexander.i.blair@gmail.com>
2022-07-14 00:09:05 +0000

    Add FormulationFactory to create TransientFormulation objects in time domain solves

--- src/hephaestus_lib/hephaestus_transient.hpp
+++ src/hephaestus_lib/hephaestus_transient.hpp
@@ -1,19 +1,9 @@
-//            ---------------------------------------------------
-//            ESolver:  Low-Frequency Electrodynamics Simulation
-//            ---------------------------------------------------
-//
-// This miniapp solves low frequency magnetodynamics problems using the E
-// formulation.
-//
-// (ν∇×E, ∇×E') - (σE, E') - (J0, E') - <(ν∇×E) × n, E'> = 0
-// -(J0, ∇ V') + <n.J, V'> = 0
+// Solves a time-domain formulation->
+// TODO: Add to Executioners?
 
 #pragma once
 #include "../common/pfem_extras.hpp"
-#include "eb_dual_solver.hpp"
-#include "hform_solver.hpp"
-#include "hj_dual_solver.hpp"
-
+#include "factory.hpp"
 #include "inputs.hpp"
 
 void transient_solve(int argc, char *argv[], hephaestus::Inputs inputs);
