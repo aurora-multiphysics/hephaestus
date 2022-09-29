c91f35bcd3520251c4f1724b298ef663eca8347e
Alexander Blair <alexander.blair@ukaea.uk>
2022-09-29 10:59:58 +0000

    Split out step from TransientExecutioner::Solve

--- src/hephaestus_lib/executioner.cpp
+++ src/hephaestus_lib/executioner.cpp
@@ -4,13 +4,12 @@ namespace hephaestus {
 
 TransientExecutioner::TransientExecutioner(
     const hephaestus::InputParameters &params)
-    : dt(params.GetParam<float>("TimeStep")),
+    : t_step(params.GetParam<float>("TimeStep")),
       t_initial(params.GetParam<float>("StartTime")),
       t_final(params.GetParam<float>("EndTime")), t(t_initial), vis_steps(1),
       visualization(true), last_step(false) {}
 
-void TransientExecutioner::Init(
-    const hephaestus::InputParameters &params) {
+void TransientExecutioner::Init(const hephaestus::InputParameters &params) {
   // Read in inputs, and initialise solver
   pmesh = new mfem::ParMesh(params.GetParam<mfem::ParMesh>("Mesh"));
   int order(params.GetParam<int>("Order"));
@@ -24,16 +23,19 @@ void TransientExecutioner::Init(
       params.GetParam<hephaestus::AuxKernels>("AuxKernels"));
   postprocessors = new hephaestus::Postprocessors(
       params.GetParam<hephaestus::Postprocessors>("Postprocessors"));
-  outputs = new hephaestus::Outputs(params.GetParam<hephaestus::Outputs>("Outputs"));
+  outputs =
+      new hephaestus::Outputs(params.GetParam<hephaestus::Outputs>("Outputs"));
+  data_collections = new std::map<std::string, mfem::DataCollection *>(
+      outputs->data_collections);
+
   std::string formulation_name(params.GetParam<std::string>("FormulationName"));
 
-  formulation =
-      hephaestus::Factory::createTransientFormulation(
-          formulation_name, *pmesh, order, variables->gfs, *bc_map,
-          *domain_properties);
+  formulation = hephaestus::Factory::createTransientFormulation(
+      formulation_name, *pmesh, order, variables->gfs, *bc_map,
+      *domain_properties);
 
   F = new mfem::BlockVector(formulation->true_offsets); // Vector of dofs
-  formulation->Init(*F);                           // Set up initial conditions
+  formulation->Init(*F); // Set up initial conditions
 
   formulation->SetTime(t);
   ode_solver = new mfem::BackwardEulerSolver;
@@ -42,15 +44,11 @@ void TransientExecutioner::Init(
   variables->Init(*pmesh);
   auxkernels->Init(variables->gfs, *domain_properties);
   postprocessors->Init(variables->gfs, *domain_properties);
-}
 
-void TransientExecutioner::Solve() const {
   auxkernels->Solve(t);
 
   // Set up DataCollections to track fields of interest.
-  std::map<std::string, mfem::DataCollection *> data_collections(
-      outputs->data_collections);
-  for (auto const &[name, dc_] : data_collections) {
+  for (auto const &[name, dc_] : *data_collections) {
     formulation->RegisterOutputFields(dc_);
     // Write initial fields to disk
     formulation->WriteOutputFields(dc_, 0);
@@ -62,128 +60,49 @@ void TransientExecutioner::Solve() const {
     formulation->InitializeGLVis();
     formulation->DisplayToGLVis();
   }
-  // Begin time evolution
+
+  // Initialise time variables
   t = t_initial;
   last_step = false;
-  for (int it = 1; !last_step; it++) {
-    // Check if current time step is final
-    if (t + dt >= t_final - dt / 2) {
-      last_step = true;
+}
+
+void TransientExecutioner::Step(double dt, int it) const {
+  // Check if current time step is final
+  if (t + dt >= t_final - dt / 2) {
+    last_step = true;
+  }
+
+  // Advance time step.
+  ode_solver->Step(*F, t, dt);
+
+  // Output data
+  if (last_step || (it % vis_steps) == 0) {
+
+    // Output timestep summary to console
+    formulation->WriteConsoleSummary(t, it);
+
+    // Make sure all ranks have sent their 'v' solution before initiating
+    // another set of GLVis connections (one from each rank):
+    MPI_Barrier(pmesh->GetComm());
+    auxkernels->Solve(t);
+
+    // Send output fields to GLVis for visualisation
+    if (visualization) {
+      formulation->DisplayToGLVis();
     }
 
-    // Advance time step.
-    ode_solver->Step(*F, t, dt);
-
-    // Output data
-    if (last_step || (it % vis_steps) == 0) {
-
-      // Output timestep summary to console
-      formulation->WriteConsoleSummary(t, it);
-
-      // Make sure all ranks have sent their 'v' solution before initiating
-      // another set of GLVis connections (one from each rank):
-      MPI_Barrier(pmesh->GetComm());
-      auxkernels->Solve(t);
-
-      // Send output fields to GLVis for visualisation
-      if (visualization) {
-        formulation->DisplayToGLVis();
-      }
-
-      // Save output fields at timestep to DataCollections
-      for (auto const &[name, dc_] : data_collections) {
-        formulation->WriteOutputFields(dc_, it);
-      }
-      postprocessors->Update(t);
+    // Save output fields at timestep to DataCollections
+    for (auto const &[name, dc_] : *data_collections) {
+      formulation->WriteOutputFields(dc_, it);
     }
+    postprocessors->Update(t);
   }
 }
 
-// void TransientExecutioner::Solve(
-//     const hephaestus::InputParameters &params) const {
-//   // Read in inputs, and initialise solver
-//   mfem::ParMesh pmesh(params.GetParam<mfem::ParMesh>("Mesh"));
-//   int order(params.GetParam<int>("Order"));
-//   hephaestus::BCMap bc_map(
-//       params.GetParam<hephaestus::BCMap>("BoundaryConditions"));
-//   hephaestus::DomainProperties domain_properties(
-//       params.GetParam<hephaestus::DomainProperties>("DomainProperties"));
-//   hephaestus::Variables variables(
-//       params.GetParam<hephaestus::Variables>("Variables"));
-//   hephaestus::AuxKernels auxkernels(
-//       params.GetParam<hephaestus::AuxKernels>("AuxKernels"));
-//   hephaestus::Postprocessors postprocessors(
-//       params.GetParam<hephaestus::Postprocessors>("Postprocessors"));
-//   hephaestus::Outputs outputs(params.GetParam<hephaestus::Outputs>("Outputs"));
-//   std::string formulation_name(params.GetParam<std::string>("FormulationName"));
-
-//   hephaestus::TransientFormulation *formulation =
-//       hephaestus::Factory::createTransientFormulation(
-//           formulation_name, pmesh, order, variables.gfs, bc_map,
-//           domain_properties);
-
-//   mfem::BlockVector F(formulation->true_offsets); // Vector of dofs
-//   formulation->Init(F);                           // Set up initial conditions
-
-//   formulation->SetTime(t);
-//   mfem::ODESolver *ode_solver = new mfem::BackwardEulerSolver;
-//   ode_solver->Init(*formulation);
-
-//   variables.Init(pmesh);
-//   auxkernels.Init(variables.gfs, domain_properties);
-//   auxkernels.Solve(t);
-//   postprocessors.Init(variables.gfs, domain_properties);
-
-//   // Set up DataCollections to track fields of interest.
-//   std::map<std::string, mfem::DataCollection *> data_collections(
-//       outputs.data_collections);
-//   for (auto const &[name, dc_] : data_collections) {
-//     formulation->RegisterOutputFields(dc_);
-//     // Write initial fields to disk
-//     formulation->WriteOutputFields(dc_, 0);
-//   }
-
-//   // Initialize GLVis visualization and send the initial condition
-//   // by socket to a GLVis server.
-//   if (visualization) {
-//     formulation->InitializeGLVis();
-//     formulation->DisplayToGLVis();
-//   }
-//   // Begin time evolution
-//   t = t_initial;
-//   last_step = false;
-//   for (int it = 1; !last_step; it++) {
-//     // Check if current time step is final
-//     if (t + dt >= t_final - dt / 2) {
-//       last_step = true;
-//     }
-
-//     // Advance time step.
-//     ode_solver->Step(F, t, dt);
-
-//     // Output data
-//     if (last_step || (it % vis_steps) == 0) {
-
-//       // Output timestep summary to console
-//       formulation->WriteConsoleSummary(t, it);
-
-//       // Make sure all ranks have sent their 'v' solution before initiating
-//       // another set of GLVis connections (one from each rank):
-//       MPI_Barrier(pmesh.GetComm());
-//       auxkernels.Solve(t);
-
-//       // Send output fields to GLVis for visualisation
-//       if (visualization) {
-//         formulation->DisplayToGLVis();
-//       }
-
-//       // Save output fields at timestep to DataCollections
-//       for (auto const &[name, dc_] : data_collections) {
-//         formulation->WriteOutputFields(dc_, it);
-//       }
-//       postprocessors.Update(t);
-//     }
-//   }
-// }
+void TransientExecutioner::Solve() const {
+  for (int it = 1; !last_step; it++) {
+    Step(t_step, it);
+  }
+}
 
 } // namespace hephaestus
--- src/hephaestus_lib/executioner.hpp
+++ src/hephaestus_lib/executioner.hpp
@@ -11,21 +11,22 @@ namespace hephaestus {
 
 class TransientExecutioner {
 private:
-  mutable double dt;  // Time step
-  double t_initial;   // Start time
-  double t_final;     // End time
-  mutable double t;   // Current time
-  int vis_steps;      // Number of cyces between each output update
+  mutable double t_step; // Time step
+  double t_initial;      // Start time
+  double t_final;        // End time
+  mutable double t;      // Current time
+  int vis_steps;         // Number of cyces between each output update
   bool visualization; // Flag to control whether GLVis visualisation is required
   mutable bool last_step; // Flag to check if current step is final
 
-  mfem::ParMesh * pmesh;
-  hephaestus::BCMap * bc_map;
-  hephaestus::DomainProperties * domain_properties;
-  hephaestus::AuxKernels * auxkernels;
-  hephaestus::Postprocessors * postprocessors;
-  hephaestus::Outputs * outputs;
-  hephaestus::TransientFormulation * formulation;
+  mfem::ParMesh *pmesh;
+  hephaestus::BCMap *bc_map;
+  hephaestus::DomainProperties *domain_properties;
+  hephaestus::AuxKernels *auxkernels;
+  hephaestus::Postprocessors *postprocessors;
+  hephaestus::Outputs *outputs;
+  std::map<std::string, mfem::DataCollection *> *data_collections;
+  hephaestus::TransientFormulation *formulation;
   mfem::ODESolver *ode_solver;
   mfem::BlockVector *F;
 
@@ -38,13 +39,13 @@ public:
   // // Solve transient FE problem.
   // void Solve(const hephaestus::InputParameters &params) const;
 
-  void Solve() const;
+  void Step(double dt, int it) const;
 
+  void Solve() const;
 
   // Enable output to GLVis
   void EnableVisualisation() { visualization = true; };
-  hephaestus::Variables * variables;
-
+  hephaestus::Variables *variables;
 };
 
 } // namespace hephaestus
