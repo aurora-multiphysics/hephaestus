5f427524ed0e082b981105ea5fa98ba258418c91
Alexander Blair <alexander.blair@ukaea.uk>
2022-11-04 16:36:48 +0000

    Adds variable to track Joule heating in coupled test

--- src/hephaestus_lib/auxkernels.cpp
+++ src/hephaestus_lib/auxkernels.cpp
@@ -15,6 +15,23 @@ void AuxKernels::Solve(double t) {
   }
 }
 
+CoefficientAuxKernel::CoefficientAuxKernel(
+    const hephaestus::InputParameters &params)
+    : AuxKernel(params), var_name(params.GetParam<std::string>("VariableName")),
+      coef_name(params.GetParam<std::string>("CoefficientName")) {}
+
+void CoefficientAuxKernel::Init(
+    const mfem::NamedFieldsMap<mfem::ParGridFunction> &variables,
+    hephaestus::DomainProperties &domain_properties) {
+  gf = variables.Get(var_name);
+  coeff = domain_properties.scalar_property_map[coef_name];
+}
+
+void CoefficientAuxKernel::Solve(double t) {
+  coeff->SetTime(t);
+  gf->ProjectCoefficient(*coeff);
+}
+
 VectorCoefficientAuxKernel::VectorCoefficientAuxKernel(
     const hephaestus::InputParameters &params)
     : AuxKernel(params), var_name(params.GetParam<std::string>("VariableName")),
