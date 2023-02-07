d611111246e6ca5ecc07a6c3aa6ab946b6c2a45c
bill ellis <Billellis.07@gmail.com>
2023-02-07 14:04:35 +0000

    Removed more build files that were accidentally added

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/apollo/contrib/hephaestus
-# Build directory: /opt/apollo/contrib/hephaestus
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
