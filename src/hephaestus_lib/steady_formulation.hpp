1e943f152a81cd7261ef9b53d5f5fca3858ecc8e
bill ellis <Billellis.07@gmail.com>
2023-02-09 22:28:18 +0000

    Created SteadyFormulation, which inherits from mfem::operator, not TimeDependentOperator

---
+++ src/hephaestus_lib/steady_formulation.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "../common/pfem_extras.hpp"
+#include "hephaestus_solvers.hpp"
+#include "inputs.hpp"
+#include "sources.hpp"
+
+namespace hephaestus {
+// Specifies output interfaces of a time-domain EM formulation.
+class SteadyFormulation : public mfem::Operator {
+
+public:
+  SteadyFormulation(){};
+
+  ~SteadyFormulation(){};
+
+  virtual void Init(mfem::Vector &X){};
+
+  virtual void RegisterVariables(){};
+
+  virtual void RegisterOutputFields(mfem::DataCollection *dc_){};
+
+  virtual void WriteOutputFields(mfem::DataCollection *dc_, int it = 0){};
+
+  virtual void WriteConsoleSummary(double t, int it){};
+
+  virtual void InitializeGLVis(){};
+
+  virtual void DisplayToGLVis(){};
+
+  mfem::Array<int> true_offsets;
+};
+} // namespace hephaestus
