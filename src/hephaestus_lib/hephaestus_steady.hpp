984d7a0f82b082e43eae5573a9be537e16f8582c
bill ellis <Billellis.07@gmail.com>
2023-02-09 22:30:26 +0000

    Added hephaestus_steady to lib, has steady state includes

---
+++ src/hephaestus_lib/hephaestus_steady.hpp
@@ -0,0 +1,8 @@
+// Header file for transient formulations.
+
+#pragma once
+#include "../common/pfem_extras.hpp"
+#include "steady_exec.hpp"
+#include "executioner.hpp"
+#include "factory.hpp"
+#include "inputs.hpp"
