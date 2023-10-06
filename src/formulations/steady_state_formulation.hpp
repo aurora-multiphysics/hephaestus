a7fa4fc1fdd7db02fe8549d8a896259d644757aa
billellis <billellis.07@gmail.com>
2023-10-06 15:05:34 +0100

    Started implementation of steady state solvers, equation systemss and formulations.

---
+++ src/formulations/steady_state_formulation.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "steady_state_problem_builder.hpp"
+
+namespace hephaestus {
+
+// 
+class SteadyStateFormulation : public hephaestus::SteadyStateProblemBuilder {
+    
+    
+public:
+    SteadyStateFormulation();
+}; 
+} // namespace hephaestus 
