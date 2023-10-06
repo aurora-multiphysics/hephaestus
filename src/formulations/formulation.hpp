4bdfea9cc4eec2a6c3d90f1271286a48cdb9933b
billellis <billellis.07@gmail.com>
2023-10-06 15:09:39 +0100

    Added SteadyStateFormulation include header to formulation.hpp.

--- src/formulations/formulation.hpp
+++ src/formulations/formulation.hpp
@@ -1,4 +1,10 @@
 #pragma once
+<<<<<<< HEAD
 #include "frequency_domain_em_formulation.hpp"
 #include "steady_state_em_formulation.hpp"
 #include "time_domain_em_formulation.hpp"
+=======
+#include "frequency_domain_formulation.hpp"
+#include "time_domain_formulation.hpp"
+#include "steady_state_formulation.hpp"
+>>>>>>> d27c7c4 (Added SteadyStateFormulation include header to formulation.hpp.)
