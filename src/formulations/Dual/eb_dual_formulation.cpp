73a03273339e92e719c9682e829f3bc2c1c7563d
Alexander Blair <alexander.blair@ukaea.uk>
2023-10-20 15:08:26 +0100

    Add EM AuxSolvers to dual formulations

--- src/formulations/Dual/eb_dual_formulation.cpp
+++ src/formulations/Dual/eb_dual_formulation.cpp
@@ -11,6 +11,44 @@ EBDualFormulation::EBDualFormulation(
                       e_field_name, b_field_name),
       _magnetic_permeability_name(magnetic_permeability_name) {}
 
+void EBDualFormulation::registerCurrentDensityAux(
+    const std::string &j_field_name) {
+  //* Current density J = Jᵉ + σE
+  //* Induced electric field, Jind = σE
+  hephaestus::AuxSolvers &auxsolvers = this->GetProblem()->postprocessors;
+  auxsolvers.Register(
+      j_field_name,
+      new hephaestus::ScaledVectorGridFunctionAux(
+          _h_curl_var_name, j_field_name, _electric_conductivity_name),
+      true);
+}
+
+void EBDualFormulation::registerLorentzForceDensityAux(
+    const std::string &f_field_name, const std::string &b_field_name,
+    const std::string &j_field_name) {
+  //* Lorentz force density = J x B
+  hephaestus::AuxSolvers &auxsolvers = this->GetProblem()->postprocessors;
+  auxsolvers.Register(
+      f_field_name,
+      new hephaestus::VectorGridFunctionCrossProductAux(
+          f_field_name, f_field_name, j_field_name, b_field_name),
+      true);
+  auxsolvers.Get(f_field_name)->SetPriority(2);
+}
+
+void EBDualFormulation::registerJouleHeatingDensityAux(
+    const std::string &p_field_name, const std::string &e_field_name,
+    const std::string &conductivity_coef_name) {
+  //* Joule heating density = E.J
+  hephaestus::AuxSolvers &auxsolvers = this->GetProblem()->postprocessors;
+  auxsolvers.Register(p_field_name,
+                      new hephaestus::VectorGridFunctionDotProductAux(
+                          p_field_name, p_field_name, e_field_name,
+                          e_field_name, _electric_conductivity_name),
+                      true);
+  auxsolvers.Get(p_field_name)->SetPriority(2);
+}
+
 void EBDualFormulation::RegisterCoefficients() {
   hephaestus::Coefficients &coefficients = this->GetProblem()->coefficients;
   if (!coefficients.scalars.Has(_magnetic_permeability_name)) {
