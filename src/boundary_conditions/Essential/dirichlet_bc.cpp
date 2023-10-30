59004e29c0c641f558fccf93bafd3139e7e656ad
billellis <billellis.07@gmail.com>
2023-10-30 23:11:19 +0000

    Added in constant value dirichlet bc's

---
+++ src/boundary_conditions/Essential/dirichlet_bc.cpp
@@ -0,0 +1,22 @@
+#include "dirichlet_bc.hpp"
+
+namespace hephaestus {
+
+DirichletBC::DirichletBC(const std::string &name_,
+                                         mfem::Array<int> bdr_attributes_)
+    : EssentialBC(name_, bdr_attributes_) {}
+
+DirichletBC::DirichletBC(const std::string &name_,
+                                         mfem::Array<int> bdr_attributes_,
+                                         mfem::FunctionCoefficient *coeff_,
+                                         mfem::FunctionCoefficient *coeff_im_)
+    : EssentialBC(name_, bdr_attributes_), coeff(coeff_), coeff_im(coeff_im_) {}
+
+void DirichletBC::applyBC(mfem::GridFunction &gridfunc,
+                                  mfem::Mesh *mesh_) {
+  mfem::Array<int> ess_bdrs(mesh_->bdr_attributes.Max());
+  ess_bdrs = this->getMarkers(*mesh_);
+  gridfunc.ProjectBdrCoefficient(*(this->coeff), ess_bdrs);
+}
+
+} // namespace hephaestus
---
+++ src/boundary_conditions/Essential/dirichlet_bc.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "essential_bc_base.hpp"
+
+namespace hephaestus{
+
+class DirichletBC : public EssentialBC{
+
+  DirichletBC(const std::string &name_,
+                      mfem::Array<int> bdr_attributes_);
+                      
+  DirichletBC(const std::string &name_,
+                      mfem::Array<int> bdr_attributes_,
+                      mfem::ConstantCoefficient *coeff_,
+                      mfem::ConstantCoefficient *coeff_im_ = nullptr);
+
+  virtual void applyBC(mfem::GridFunction &gridfunc,
+                       mfem::Mesh *mesh_) override;
+
+  mfem::ConstantCoefficient *coeff;
+  mfem::ConstantCoefficient *coeff_im;
+};
+} // namespace hephaestus
