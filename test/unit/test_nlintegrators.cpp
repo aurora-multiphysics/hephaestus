1ee496ae5ace1c8915798afc3cdeda58f0666f05
Alexander Blair <alexander.blair@ukaea.uk>
2023-11-17 19:31:10 +0000

    First version with working Newton solver

--- test/unit/test_nlintegrators.cpp
+++ test/unit/test_nlintegrators.cpp
@@ -106,8 +106,60 @@ public:
   };
 };
 
+/** NonlinearOperator operator of the form:
+    k --> (M + dt*S)*k + H(x + dt*v + dt^2*k) + S*v,
+    where M and S are given BilinearForms, H is a given NonlinearForm, v and x
+    are given vectors, and dt is a scalar. */
+class NonlinearOperator : public mfem::Operator {
+private:
+  mfem::ParBilinearForm *blf;
+  mfem::ParNonlinearForm *nlf;
+  mutable mfem::HypreParMatrix *Jacobian;
+  double dt;
+  const mfem::Vector *x0;
+  mutable mfem::Vector x1;
+  const mfem::Array<int> &ess_tdof_list;
+
+public:
+  NonlinearOperator(mfem::ParBilinearForm *blf_, mfem::ParNonlinearForm *nlf_,
+                    const mfem::Array<int> &ess_tdof_list_)
+      : Operator(blf_->ParFESpace()->TrueVSize()), blf(blf_), nlf(nlf_),
+        Jacobian(NULL), dt(0.0), x0(NULL), x1(height),
+        ess_tdof_list(ess_tdof_list_){};
+
+  /// Set current dt, v, x values - needed to compute action and Jacobian.
+  void SetParameters(double dt_, const mfem::Vector *x0_) {
+    dt = dt_;
+    x0 = x0_;
+  };
+
+  //* residual = (ρ(∇×H), ∇×v) + (μdH/dt, v) + (dBᵉ/dt, v) - <(ρ∇×H)×n, v>  = 0
+  /// Compute y = H(x0 + dt* dx/dt) + M dx/dt
+  virtual void Mult(const mfem::Vector &dx_dt, mfem::Vector &residual) const {
+    add(*x0, dt, dx_dt, x1);
+    nlf->Mult(x1, residual);
+    blf->TrueAddMult(dx_dt, residual);
+  };
+
+  /// Compute J = dy/d(dx/dt)
+  /// Compute J = M + dt grad_H(x0 + dt* dx/dt)
+  virtual mfem::Operator &GetGradient(const mfem::Vector &dx_dt) const {
+    add(*x0, dt, dx_dt, x1);
+    delete Jacobian;
+    mfem::SparseMatrix &localJ = blf->SpMat();
+    localJ.Add(dt, nlf->GetLocalGradient(x1));
+    Jacobian = blf->ParallelAssemble(&localJ);
+    mfem::HypreParMatrix *Je = Jacobian->EliminateRowsCols(ess_tdof_list);
+    delete Je;
+    return *Jacobian;
+  };
+
+  virtual ~NonlinearOperator() { delete Jacobian; };
+};
+
 TEST(NonlinearIntegratorTest, CheckData) {
-  mfem::Mesh mesh((std::string(DATA_DIR) + "coil.gen").c_str(), 1, 1);
+  mfem::Mesh mesh((std::string(DATA_DIR) + "cylinder-hex-q2.gen").c_str(), 1,
+                  1);
   std::shared_ptr<mfem::ParMesh> pmesh =
       std::make_shared<mfem::ParMesh>(mfem::ParMesh(MPI_COMM_WORLD, mesh));
 
@@ -118,30 +170,68 @@ TEST(NonlinearIntegratorTest, CheckData) {
   fec_ND = new mfem::ND_FECollection(1, pmesh->Dimension());
   fec_RT = new mfem::RT_FECollection(1, pmesh->Dimension());
   mfem::ParFiniteElementSpace HCurlFESpace(pmesh.get(), fec_ND);
-  mfem::ParFiniteElementSpace HDivFESpace(pmesh.get(), fec_RT);
   mfem::ConstantCoefficient coeff(1.0);
-
-  // Build the NonlinearForm
-  const int vdim = 1;
+  mfem::ConstantCoefficient mu(1.0);
 
   //* in weak form
   //* (ρ∇×H, ∇×v) + (μdH/dt, v) + (dBᵉ/dt, v) - <(ρ∇×H)×n, v>  = 0
-  mfem::ParNonlinearForm k_test(&HCurlFESpace);
-  k_test.AddDomainIntegrator(new VectorPowerLawNLFIntegrator(coeff));
-  // k_test.Assemble();
-  k_test.Setup();
+  mfem::ParNonlinearForm nlf_test(&HCurlFESpace);
+  mfem::ParBilinearForm blf_test(&HCurlFESpace);
+  mfem::ParBilinearForm lf_test(&HCurlFESpace);
+  nlf_test.AddDomainIntegrator(new VectorPowerLawNLFIntegrator(coeff));
+  blf_test.AddDomainIntegrator(new mfem::VectorFEMassIntegrator(mu));
 
-  // Compare ceed with mfem.
+  //* Assemble and finalize
+  nlf_test.Setup();
+  blf_test.Assemble();
+  blf_test.Finalize();
 
   mfem::ParGridFunction x(&HCurlFESpace);
   mfem::ParLinearForm b(&HCurlFESpace);
-  b = 0.0;
+  b = 1.0;
+  x = 0.0;
   mfem::Vector X(HCurlFESpace.GetTrueVSize()), B(HCurlFESpace.GetTrueVSize());
   b.ParallelAssemble(B);
   x.ParallelProject(X);
 
-  k_test.Mult(B, X);
+  mfem::Array<int> ess_tdof_list;
+  NonlinearOperator nl_oper(&blf_test, &nlf_test, ess_tdof_list);
+  nl_oper.SetParameters(0.1, &X);
+
+  // Solver for the Jacobian solve in the Newton method
+  mfem::Solver *jacobian_solver;
+  // Set up the Jacobian solver
+  mfem::HyprePCG j_pcg(HCurlFESpace.GetComm());
+  // mfem::HypreAMS ams(&HCurlFESpace);
+  // ams.SetSingularProblem();
+  mfem::HypreBoomerAMG ams;
+  ams.SetPrintLevel(1);
+  j_pcg.SetTol(1e-7);
+  j_pcg.SetMaxIter(300);
+  j_pcg.SetPrintLevel(1);
+  j_pcg.SetPreconditioner(ams);
+  jacobian_solver = &j_pcg;
+
+  // Newton solver for the hyperelastic operator
+  mfem::NewtonSolver newton_solver;
+  newton_solver.iterative_mode = true;
+  newton_solver.SetSolver(*jacobian_solver);
+  newton_solver.SetOperator(nl_oper);
+  newton_solver.SetPrintLevel(1); // print Newton iterations
+  newton_solver.SetRelTol(0.0);
+  newton_solver.SetAbsTol(1e-14);
+  // newton_solver.SetAdaptiveLinRtol(2, 0.5, 0.9);
+  newton_solver.SetMaxIter(10);
+
+  int _my_rank;
+  MPI_Comm_rank(HCurlFESpace.GetComm(), &_my_rank);
+  std::cout << "Starting on rank:" << _my_rank << std::endl;
+
+  // nlf_test.Mult(B, X);
+  newton_solver.Mult(B, X);
+  MFEM_VERIFY(newton_solver.GetConverged(), "Newton Solver did not converge.");
+  std::cout << "Finished on rank:" << _my_rank << std::endl;
   x.Distribute(X);
 
-  EXPECT_LT(x.Norml2(), 1.e-12);
+  // EXPECT_LT(x.Norml2(), 1.e-12);
 }
