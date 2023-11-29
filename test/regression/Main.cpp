c203a6598bb7da5cc2f0b16896159e487de4f1b5
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2023-11-29 18:49:10 +0000

    Integration tests

--- test/regression/Main.cpp
+++ test/regression/Main.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 #include <catch2/catch_session.hpp>
 
+const char *DATA_DIR = "../data/";
+
 int main(int argc, char *argv[]) {
   std::cout << "Regression tests from test/regression/Main.cpp" << std::endl;
   int result = 0;
-  mfem::OptionsParser args(argc, argv);
-  args.AddOption(&DATA_DIR, "-dataDir", "--data_directory",
-                 "Directory storing input data for tests.");
-  args.Parse();
   MPI_Init(&argc, &argv);
   result = Catch::Session().run( argc, argv );
   MPI_Finalize();
