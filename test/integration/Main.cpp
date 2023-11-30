aa22120fbef926a5178a004af0fe7a9790f1241e
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2023-11-30 13:31:21 +0000

    Fixed "ghost" tests

--- test/integration/Main.cpp
+++ test/integration/Main.cpp
@@ -5,14 +5,14 @@
 const char *DATA_DIR = "../data/";
 
 int main(int argc, char *argv[]) {
-  std::cout << "Integration tests from test/integration/Main.cpp" << std::endl;
-  int result = 0;
+
   mfem::OptionsParser args(argc, argv);
   args.AddOption(&DATA_DIR, "-dataDir", "--data_directory",
                  "Directory storing input data for tests.");
   args.Parse();
   MPI_Init(&argc, &argv);
-  result = Catch::Session().run( argc, argv );
+  int result = Catch::Session().run( argc, argv );
   MPI_Finalize();
+  
   return result;
 }
--- test/regression/Main.cpp
+++ test/regression/Main.cpp
@@ -5,10 +5,10 @@
 const char *DATA_DIR = "../data/";
 
 int main(int argc, char *argv[]) {
-  std::cout << "Regression tests from test/regression/Main.cpp" << std::endl;
-  int result = 0;
+
   MPI_Init(&argc, &argv);
-  result = Catch::Session().run( argc, argv );
+  int result = Catch::Session().run( argc, argv );
   MPI_Finalize();
+  
   return result;
 }
