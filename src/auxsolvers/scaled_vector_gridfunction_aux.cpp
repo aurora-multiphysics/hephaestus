262279000c7875d66dc902bb88220b9b8c2a6c52
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 16:55:22 +0000

    updated scaled_vector_gridfunction_aux, vector_coefficient_aux, vector_gridfunction_cross_product_aux, vector_gridfunction_dot_product_aux.

--- src/auxsolvers/scaled_vector_gridfunction_aux.cpp
+++ src/auxsolvers/scaled_vector_gridfunction_aux.cpp
@@ -1,50 +1,71 @@
 #include "scaled_vector_gridfunction_aux.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
 ScaledVectorGridFunctionAux::ScaledVectorGridFunctionAux(
-    const std::string &input_gf_name, const std::string &scaled_gf_name,
-    const std::string &coef_name, const double &aConst,
-    const hephaestus::InputParameters &solver_options)
-    : AuxSolver(), _input_gf_name(input_gf_name),
-      _scaled_gf_name(scaled_gf_name), _coef_name(coef_name), _aConst(aConst),
-      _solver_options(solver_options), coef(nullptr), input_gf(nullptr),
-      scaled_gf(nullptr), test_fes(nullptr), trial_fes(nullptr), a(nullptr),
-      a_mixed(nullptr), a_mat(nullptr), mixed_mat(nullptr), solver(nullptr) {}
+    const std::string & input_gf_name,
+    const std::string & scaled_gf_name,
+    const std::string & coef_name,
+    const double & aConst,
+    const hephaestus::InputParameters & solver_options)
+  : AuxSolver(),
+    _input_gf_name(input_gf_name),
+    _scaled_gf_name(scaled_gf_name),
+    _coef_name(coef_name),
+    _aConst(aConst),
+    _solver_options(solver_options),
+    coef(nullptr),
+    input_gf(nullptr),
+    scaled_gf(nullptr),
+    test_fes(nullptr),
+    trial_fes(nullptr),
+    a(nullptr),
+    a_mixed(nullptr),
+    a_mat(nullptr),
+    mixed_mat(nullptr),
+    solver(nullptr)
+{
+}
 
-void ScaledVectorGridFunctionAux::Init(
-    const hephaestus::GridFunctions &gridfunctions,
-    hephaestus::Coefficients &coefficients) {
+void
+ScaledVectorGridFunctionAux::Init(const hephaestus::GridFunctions & gridfunctions,
+                                  hephaestus::Coefficients & coefficients)
+{
   input_gf = gridfunctions.Get(_input_gf_name);
-  if (input_gf == NULL) {
-    MFEM_ABORT("GridFunction "
-               << _input_gf_name
-               << " not found when initializing ScaledVectorGridFunctionAux");
+  if (input_gf == NULL)
+  {
+    MFEM_ABORT("GridFunction " << _input_gf_name
+                               << " not found when initializing ScaledVectorGridFunctionAux");
   }
   scaled_gf = gridfunctions.Get(_scaled_gf_name);
-  if (scaled_gf == NULL) {
-    MFEM_ABORT("GridFunction "
-               << _scaled_gf_name
-               << " not found when initializing ScaledVectorGridFunctionAux");
+  if (scaled_gf == NULL)
+  {
+    MFEM_ABORT("GridFunction " << _scaled_gf_name
+                               << " not found when initializing ScaledVectorGridFunctionAux");
   }
-  coef =
-      dynamic_cast<mfem::Coefficient *>(coefficients.scalars.Get(_coef_name));
-  if (coef == NULL) {
-    MFEM_ABORT("Coefficient "
-               << _coef_name
-               << " not found when initializing ScaledVectorGridFunctionAux");
+  coef = dynamic_cast<mfem::Coefficient *>(coefficients.scalars.Get(_coef_name));
+  if (coef == NULL)
+  {
+    MFEM_ABORT("Coefficient " << _coef_name
+                              << " not found when initializing ScaledVectorGridFunctionAux");
   }
 
   test_fes = scaled_gf->ParFESpace();
   trial_fes = input_gf->ParFESpace();
+
   buildBilinearForm();
   buildMixedBilinearForm();
+
   a_mat = a->ParallelAssemble();
   solver = new hephaestus::DefaultJacobiPCGSolver(_solver_options, *a_mat);
 }
 
-void ScaledVectorGridFunctionAux::buildBilinearForm() {
-  if (a != nullptr) {
+void
+ScaledVectorGridFunctionAux::buildBilinearForm()
+{
+  if (a != nullptr)
+  {
     delete a;
   }
   a = new mfem::ParBilinearForm(test_fes);
@@ -53,8 +74,11 @@ void ScaledVectorGridFunctionAux::buildBilinearForm() {
   a->Finalize();
 }
 
-void ScaledVectorGridFunctionAux::buildMixedBilinearForm() {
-  if (a_mixed != nullptr) {
+void
+ScaledVectorGridFunctionAux::buildMixedBilinearForm()
+{
+  if (a_mixed != nullptr)
+  {
     delete a_mixed;
   }
   a_mixed = new mfem::ParMixedBilinearForm(trial_fes, test_fes);
@@ -63,7 +87,9 @@ void ScaledVectorGridFunctionAux::buildMixedBilinearForm() {
   a_mixed->Finalize();
 }
 
-void ScaledVectorGridFunctionAux::Solve(double t) {
+void
+ScaledVectorGridFunctionAux::Solve(double t)
+{
   mfem::Vector B(test_fes->GetTrueVSize());  // Linear form true DOFs
   mfem::Vector X(test_fes->GetTrueVSize());  // H(Div) gridfunction true DOFs
   mfem::Vector P(trial_fes->GetTrueVSize()); // H(Curl) gridfunction true DOFs
@@ -84,7 +110,8 @@ void ScaledVectorGridFunctionAux::Solve(double t) {
   scaled_gf->SetFromTrueDofs(X);
 }
 
-ScaledVectorGridFunctionAux::~ScaledVectorGridFunctionAux() {
+ScaledVectorGridFunctionAux::~ScaledVectorGridFunctionAux()
+{
   if (a != nullptr)
     delete a;
   if (a_mixed != nullptr)
