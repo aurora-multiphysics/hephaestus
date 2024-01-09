15d0d11dbc811577e326bd0581b9aff7dae08e6b
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 16:57:50 +0000

    Updated formatting for all boundary condition files.

--- src/boundary_conditions/boundary_conditions.hpp
+++ src/boundary_conditions/boundary_conditions.hpp
@@ -3,30 +3,33 @@
 #include "integrated_bcs.hpp"
 #include "robin_bcs.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
-class BCMap : public mfem::NamedFieldsMap<hephaestus::BoundaryCondition> {
+class BCMap : public mfem::NamedFieldsMap<hephaestus::BoundaryCondition>
+{
 public:
-  mfem::Array<int> getEssentialBdrMarkers(const std::string &name_,
-                                          mfem::Mesh *mesh_);
+  mfem::Array<int> getEssentialBdrMarkers(const std::string & name_, mfem::Mesh * mesh_);
 
-  void applyEssentialBCs(const std::string &name_,
-                         mfem::Array<int> &ess_tdof_list,
-                         mfem::GridFunction &gridfunc, mfem::Mesh *mesh_);
+  void applyEssentialBCs(const std::string & name_,
+                         mfem::Array<int> & ess_tdof_list,
+                         mfem::GridFunction & gridfunc,
+                         mfem::Mesh * mesh_);
 
-  void applyEssentialBCs(const std::string &name_,
-                         mfem::Array<int> &ess_tdof_list,
-                         mfem::ParComplexGridFunction &gridfunc,
-                         mfem::Mesh *mesh_);
+  void applyEssentialBCs(const std::string & name_,
+                         mfem::Array<int> & ess_tdof_list,
+                         mfem::ParComplexGridFunction & gridfunc,
+                         mfem::Mesh * mesh_);
 
-  void applyIntegratedBCs(const std::string &name_, mfem::LinearForm &lf,
-                          mfem::Mesh *mesh_);
+  void applyIntegratedBCs(const std::string & name_, mfem::LinearForm & lf, mfem::Mesh * mesh_);
 
-  void applyIntegratedBCs(const std::string &name_,
-                          mfem::ParComplexLinearForm &clf, mfem::Mesh *mesh_);
+  void applyIntegratedBCs(const std::string & name_,
+                          mfem::ParComplexLinearForm & clf,
+                          mfem::Mesh * mesh_);
 
-  void applyIntegratedBCs(const std::string &name_,
-                          mfem::ParSesquilinearForm &clf, mfem::Mesh *mesh_);
+  void applyIntegratedBCs(const std::string & name_,
+                          mfem::ParSesquilinearForm & clf,
+                          mfem::Mesh * mesh_);
 };
 
 } // namespace hephaestus
