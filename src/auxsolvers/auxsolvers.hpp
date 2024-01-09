9236358b6848be5f7c0da865bfea35245f18860c
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 16:43:59 +0000

    Reformatted auxsolvers_base and auxsolvers.

--- src/auxsolvers/auxsolvers.hpp
+++ src/auxsolvers/auxsolvers.hpp
@@ -12,14 +12,16 @@
 
 // Specify classes that perform auxiliary calculations on GridFunctions or
 // Coefficients.
-namespace hephaestus {
+namespace hephaestus
+{
 
-class AuxSolvers : public mfem::NamedFieldsMap<hephaestus::AuxSolver> {
+class AuxSolvers : public mfem::NamedFieldsMap<hephaestus::AuxSolver>
+{
 private:
 public:
   std::vector<hephaestus::AuxSolver *> aux_queue;
-  void Init(const hephaestus::GridFunctions &gridfunctions,
-            hephaestus::Coefficients &coefficients);
+  void Init(const hephaestus::GridFunctions & gridfunctions,
+            hephaestus::Coefficients & coefficients);
   void Solve(double t = 0.0);
 };
 
