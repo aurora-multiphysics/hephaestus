b956e91c723599de6e817cfc144ae2e336bd3fb5
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 17:06:52 +0000

    Updated gridfunctions formatting.

--- src/gridfunctions/gridfunctions.hpp
+++ src/gridfunctions/gridfunctions.hpp
@@ -1,13 +1,19 @@
 #pragma once
 #include "mfem.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
-class FECollections
-    : public mfem::NamedFieldsMap<mfem::FiniteElementCollection> {};
+class FECollections : public mfem::NamedFieldsMap<mfem::FiniteElementCollection>
+{
+};
 
-class FESpaces : public mfem::NamedFieldsMap<mfem::ParFiniteElementSpace> {};
+class FESpaces : public mfem::NamedFieldsMap<mfem::ParFiniteElementSpace>
+{
+};
 
-class GridFunctions : public mfem::NamedFieldsMap<mfem::ParGridFunction> {};
+class GridFunctions : public mfem::NamedFieldsMap<mfem::ParGridFunction>
+{
+};
 
 } // namespace hephaestus
