c3f4c90da0ea01c563a7b3421bbe2b53d05380d5
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 17:13:05 +0000

    Updated examples formatting.

--- examples/complex_team7/Main.cpp
+++ examples/complex_team7/Main.cpp
@@ -1,8 +1,10 @@
 #include "hephaestus.hpp"
 
-const char *DATA_DIR = "../../data/";
+const char * DATA_DIR = "../../data/";
 
-static void source_current(const mfem::Vector &xv, double t, mfem::Vector &J) {
+static void
+source_current(const mfem::Vector & xv, double t, mfem::Vector & J)
+{
   double x0(194e-3);  // Coil centre x coordinate
   double y0(100e-3);  // Coil centre y coordinate
   double a(50e-3);    // Coil thickness
@@ -17,25 +19,33 @@ static void source_current(const mfem::Vector &xv, double t, mfem::Vector &J) {
   double Jmag = (I0 / S);
 
   // Calculate x component of current density unit vector
-  if (abs(x - x0) < a) {
+  if (abs(x - x0) < a)
+  {
     J(0) = -(y - y0) / abs(y - y0);
-  } else if (abs(y - y0) < a) {
+  }
+  else if (abs(y - y0) < a)
+  {
     J(0) = 0.0;
-  } else {
+  }
+  else
+  {
     J(0) = -(y - (y0 + a * ((y - y0) / abs(y - y0)))) /
-           hypot(x - (x0 + a * ((x - x0) / abs(x - x0))),
-                 y - (y0 + a * ((y - y0) / abs(y - y0))));
+           hypot(x - (x0 + a * ((x - x0) / abs(x - x0))), y - (y0 + a * ((y - y0) / abs(y - y0))));
   }
 
   // Calculate y component of current density unit vector
-  if (abs(y - y0) < a) {
+  if (abs(y - y0) < a)
+  {
     J(1) = (x - x0) / abs(x - x0);
-  } else if (abs(x - x0) < a) {
+  }
+  else if (abs(x - x0) < a)
+  {
     J(1) = 0.0;
-  } else {
+  }
+  else
+  {
     J(1) = (x - (x0 + a * ((x - x0) / abs(x - x0)))) /
-           hypot(x - (x0 + a * ((x - x0) / abs(x - x0))),
-                 y - (y0 + a * ((y - y0) / abs(y - y0))));
+           hypot(x - (x0 + a * ((x - x0) / abs(x - x0))), y - (y0 + a * ((y - y0) / abs(y - y0))));
   }
 
   // Calculate z component of current density unit vector
@@ -45,36 +55,36 @@ static void source_current(const mfem::Vector &xv, double t, mfem::Vector &J) {
   J *= Jmag;
 }
 
-hephaestus::Coefficients defineCoefficients() {
+hephaestus::Coefficients
+defineCoefficients()
+{
   hephaestus::Subdomain air("air", 1);
-  air.scalar_coefficients.Register("electrical_conductivity",
-                                   new mfem::ConstantCoefficient(1.0), true);
+  air.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
   hephaestus::Subdomain plate("plate", 2);
   plate.scalar_coefficients.Register(
       "electrical_conductivity", new mfem::ConstantCoefficient(3.526e7), true);
   hephaestus::Subdomain coil1("coil1", 3);
-  coil1.scalar_coefficients.Register("electrical_conductivity",
-                                     new mfem::ConstantCoefficient(1.0), true);
+  coil1.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
   hephaestus::Subdomain coil2("coil2", 4);
-  coil2.scalar_coefficients.Register("electrical_conductivity",
-                                     new mfem::ConstantCoefficient(1.0), true);
+  coil2.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
   hephaestus::Subdomain coil3("coil3", 5);
-  coil3.scalar_coefficients.Register("electrical_conductivity",
-                                     new mfem::ConstantCoefficient(1.0), true);
+  coil3.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
   hephaestus::Subdomain coil4("coil4", 6);
-  coil4.scalar_coefficients.Register("electrical_conductivity",
-                                     new mfem::ConstantCoefficient(1.0), true);
-  hephaestus::Coefficients coefficients(std::vector<hephaestus::Subdomain>(
-      {air, plate, coil1, coil2, coil3, coil4}));
-  coefficients.scalars.Register("frequency",
-                                new mfem::ConstantCoefficient(200.0), true);
-  coefficients.scalars.Register("magnetic_permeability",
-                                new mfem::ConstantCoefficient(M_PI * 4.0e-7),
-                                true);
-  coefficients.scalars.Register("dielectric_permittivity",
-                                new mfem::ConstantCoefficient(8.854e-12), true);
+  coil4.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
+  hephaestus::Coefficients coefficients(
+      std::vector<hephaestus::Subdomain>({air, plate, coil1, coil2, coil3, coil4}));
+  coefficients.scalars.Register("frequency", new mfem::ConstantCoefficient(200.0), true);
+  coefficients.scalars.Register(
+      "magnetic_permeability", new mfem::ConstantCoefficient(M_PI * 4.0e-7), true);
+  coefficients.scalars.Register(
+      "dielectric_permittivity", new mfem::ConstantCoefficient(8.854e-12), true);
 
-  mfem::VectorFunctionCoefficient *JSrcCoef =
+  mfem::VectorFunctionCoefficient * JSrcCoef =
       new mfem::VectorFunctionCoefficient(3, source_current);
   mfem::Array<mfem::VectorCoefficient *> sourcecoefs(4);
   sourcecoefs[0] = JSrcCoef;
@@ -86,13 +96,15 @@ hephaestus::Coefficients defineCoefficients() {
   coilsegments[1] = 4;
   coilsegments[2] = 5;
   coilsegments[3] = 6;
-  mfem::PWVectorCoefficient *JSrcRestricted =
+  mfem::PWVectorCoefficient * JSrcRestricted =
       new mfem::PWVectorCoefficient(3, coilsegments, sourcecoefs);
   coefficients.vectors.Register("source", JSrcRestricted, true);
   return coefficients;
 }
 
-hephaestus::Sources defineSources() {
+hephaestus::Sources
+defineSources()
+{
   hephaestus::InputParameters div_free_source_params;
   div_free_source_params.SetParam("SourceName", std::string("source"));
   div_free_source_params.SetParam("HCurlFESpaceName", std::string("HCurl"));
@@ -103,34 +115,38 @@ hephaestus::Sources defineSources() {
   current_solver_options.SetParam("PrintLevel", 0);
   div_free_source_params.SetParam("SolverOptions", current_solver_options);
   hephaestus::Sources sources;
-  sources.Register("source",
-                   new hephaestus::DivFreeSource(div_free_source_params), true);
+  sources.Register("source", new hephaestus::DivFreeSource(div_free_source_params), true);
   return sources;
 }
-hephaestus::Outputs defineOutputs() {
+hephaestus::Outputs
+defineOutputs()
+{
   hephaestus::Outputs outputs;
-  outputs.Register("ParaViewDataCollection",
-                   new mfem::ParaViewDataCollection("ComplexTeam7ParaView"),
-                   true);
+  outputs.Register(
+      "ParaViewDataCollection", new mfem::ParaViewDataCollection("ComplexTeam7ParaView"), true);
   return outputs;
 }
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char * argv[])
+{
   mfem::OptionsParser args(argc, argv);
-  args.AddOption(&DATA_DIR, "-dataDir", "--data_directory",
-                 "Directory storing input data for tests.");
+  args.AddOption(
+      &DATA_DIR, "-dataDir", "--data_directory", "Directory storing input data for tests.");
   args.Parse();
   MPI_Init(&argc, &argv);
 
   // Create Formulation
-  hephaestus::ComplexAFormulation *problem_builder =
-      new hephaestus::ComplexAFormulation(
-          "magnetic_reluctivity", "electrical_conductivity",
-          "dielectric_permittivity", "frequency", "magnetic_vector_potential",
-          "magnetic_vector_potential_real", "magnetic_vector_potential_imag");
+  hephaestus::ComplexAFormulation * problem_builder =
+      new hephaestus::ComplexAFormulation("magnetic_reluctivity",
+                                          "electrical_conductivity",
+                                          "dielectric_permittivity",
+                                          "frequency",
+                                          "magnetic_vector_potential",
+                                          "magnetic_vector_potential_real",
+                                          "magnetic_vector_potential_imag");
   // Set Mesh
-  mfem::Mesh mesh((std::string(DATA_DIR) + std::string("./team7.g")).c_str(), 1,
-                  1);
+  mfem::Mesh mesh((std::string(DATA_DIR) + std::string("./team7.g")).c_str(), 1, 1);
   std::shared_ptr<mfem::ParMesh> pmesh =
       std::make_shared<mfem::ParMesh>(mfem::ParMesh(MPI_COMM_WORLD, mesh));
   problem_builder->SetMesh(pmesh);
@@ -144,8 +160,7 @@ int main(int argc, char *argv[]) {
 
   problem_builder->AddGridFunction("electric_field_real", "HCurl");
   problem_builder->AddGridFunction("electric_field_imag", "HCurl");
-  problem_builder->registerElectricFieldAux("electric_field_real",
-                                            "electric_field_imag");
+  problem_builder->registerElectricFieldAux("electric_field_real", "electric_field_imag");
 
   problem_builder->AddGridFunction("magnetic_flux_density_real", "HDiv");
   problem_builder->AddGridFunction("magnetic_flux_density_imag", "HDiv");
@@ -154,13 +169,13 @@ int main(int argc, char *argv[]) {
 
   problem_builder->AddGridFunction("current_density_real", "HDiv");
   problem_builder->AddGridFunction("current_density_imag", "HDiv");
-  problem_builder->registerCurrentDensityAux("current_density_real",
-                                             "current_density_imag");
+  problem_builder->registerCurrentDensityAux("current_density_real", "current_density_imag");
 
   problem_builder->AddGridFunction("joule_heating_density", "L2");
-  problem_builder->registerJouleHeatingDensityAux(
-      "joule_heating_density", "electric_field_real", "electric_field_imag",
-      "electrical_conductivity");
+  problem_builder->registerJouleHeatingDensityAux("joule_heating_density",
+                                                  "electric_field_real",
+                                                  "electric_field_imag",
+                                                  "electrical_conductivity");
 
   hephaestus::Coefficients coefficients = defineCoefficients();
   problem_builder->SetCoefficients(coefficients);
@@ -179,13 +194,11 @@ int main(int argc, char *argv[]) {
 
   hephaestus::ProblemBuildSequencer sequencer(problem_builder);
   sequencer.ConstructOperatorProblem();
-  std::unique_ptr<hephaestus::SteadyStateProblem> problem =
-      problem_builder->ReturnProblem();
+  std::unique_ptr<hephaestus::SteadyStateProblem> problem = problem_builder->ReturnProblem();
 
   hephaestus::InputParameters exec_params;
   exec_params.SetParam("Problem", problem.get());
-  hephaestus::SteadyExecutioner *executioner =
-      new hephaestus::SteadyExecutioner(exec_params);
+  hephaestus::SteadyExecutioner * executioner = new hephaestus::SteadyExecutioner(exec_params);
 
   std::cout << "Created exec ";
   executioner->Execute();
--- examples/open_coil/Main.cpp
+++ examples/open_coil/Main.cpp
@@ -1,32 +1,36 @@
 #include "hephaestus.hpp"
 
-const char *DATA_DIR = "../../data/";
+const char * DATA_DIR = "../../data/";
 
-static void zeroVec(const mfem::Vector &x, mfem::Vector &V) { V = 1.0; }
+static void
+zeroVec(const mfem::Vector & x, mfem::Vector & V)
+{
+  V = 1.0;
+}
 
-hephaestus::Coefficients defineCoefficients(double Itotal) {
+hephaestus::Coefficients
+defineCoefficients(double Itotal)
+{
 
   hephaestus::Subdomain coil("coil", 1);
   coil.scalar_coefficients.Register(
       "electrical_conductivity", new mfem::ConstantCoefficient(3.526e7), true);
   hephaestus::Subdomain air("air", 2);
-  air.scalar_coefficients.Register("electrical_conductivity",
-                                   new mfem::ConstantCoefficient(1.0), true);
-  hephaestus::Coefficients coefficients(
-      std::vector<hephaestus::Subdomain>({coil, air}));
-  coefficients.scalars.Register("magnetic_permeability",
-                                new mfem::ConstantCoefficient(M_PI * 4.0e-7),
-                                true);
+  air.scalar_coefficients.Register(
+      "electrical_conductivity", new mfem::ConstantCoefficient(1.0), true);
+  hephaestus::Coefficients coefficients(std::vector<hephaestus::Subdomain>({coil, air}));
+  coefficients.scalars.Register(
+      "magnetic_permeability", new mfem::ConstantCoefficient(M_PI * 4.0e-7), true);
 
   // Time-dependent current
-  coefficients.scalars.Register("I", new mfem::ConstantCoefficient(Itotal),
-                                true);
+  coefficients.scalars.Register("I", new mfem::ConstantCoefficient(Itotal), true);
 
   return coefficients;
 }
 
-hephaestus::Sources defineSources(std::pair<int, int> elec,
-                                  mfem::Array<int> coil_domains) {
+hephaestus::Sources
+defineSources(std::pair<int, int> elec, mfem::Array<int> coil_domains)
+{
 
   hephaestus::InputParameters coilsolver_pars;
   coilsolver_pars.SetParam("SourceName", std::string("source_current_density"));
@@ -35,22 +39,23 @@ hephaestus::Sources defineSources(std::pair<int, int> elec,
 
   hephaestus::Sources sources;
   sources.Register(
-      "source",
-      new hephaestus::OpenCoilSolver(coilsolver_pars, coil_domains, elec),
-      true);
+      "source", new hephaestus::OpenCoilSolver(coilsolver_pars, coil_domains, elec), true);
   return sources;
 }
 
-hephaestus::Outputs defineOutputs() {
+hephaestus::Outputs
+defineOutputs()
+{
 
   hephaestus::Outputs outputs;
-    outputs.Register("ParaViewDataCollection",
-                     new mfem::ParaViewDataCollection("OpenCoilParaView"),
-                     true);
-    return outputs;
+  outputs.Register(
+      "ParaViewDataCollection", new mfem::ParaViewDataCollection("OpenCoilParaView"), true);
+  return outputs;
 }
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char * argv[])
+{
 
   // Refinement and order
   int par_ref_lvl = -1;
@@ -73,17 +78,19 @@ int main(int argc, char *argv[]) {
   std::string elec_bdr_attr = "1 2";
 
   mfem::OptionsParser args(argc, argv);
-  args.AddOption(&DATA_DIR, "-dataDir", "--data_directory",
-                 "Directory storing input data for tests.");
-  args.AddOption(&par_ref_lvl, "-ref", "--parallel-refinement",
-                 "Parallel refinement level.");
+  args.AddOption(
+      &DATA_DIR, "-dataDir", "--data_directory", "Directory storing input data for tests.");
+  args.AddOption(&par_ref_lvl, "-ref", "--parallel-refinement", "Parallel refinement level.");
   args.AddOption(&order, "-o", "--order", "Base functions order");
   args.AddOption(&Itotal, "-I", "--Itotal", "Total electrical current.");
   args.AddOption(&mesh_filename, "-f", "--mesh-filename", "Mesh file name");
-  args.AddOption(
-      &coil_attr, "-cd", "--coil-domains",
-      "List of coil domain attributes separated by spaces, e.g. \'1 3 4\'");
-  args.AddOption(&elec_bdr_attr, "-e", "--electrode-attrs",
+  args.AddOption(&coil_attr,
+                 "-cd",
+                 "--coil-domains",
+                 "List of coil domain attributes separated by spaces, e.g. \'1 3 4\'");
+  args.AddOption(&elec_bdr_attr,
+                 "-e",
+                 "--electrode-attrs",
                  "List of electrode attributes separated by spaces, e.g. \'1 "
                  "2\'. Must be two values.");
 
@@ -117,28 +124,22 @@ int main(int argc, char *argv[]) {
     elec_bdr_array.Append(att);
 
   if (elec_bdr_array.Size() != 2)
-    mfem::mfem_error(
-        "Electrode boundary attribute list must contain two attributes.");
+    mfem::mfem_error("Electrode boundary attribute list must contain two attributes.");
 
   elec_attrs.first = elec_bdr_array[0];
   elec_attrs.second = elec_bdr_array[1];
 
   // Create Formulation
-  hephaestus::MagnetostaticFormulation *problem_builder =
-      new hephaestus::MagnetostaticFormulation("magnetic_reluctivity",
-                                               "magnetic_permeability",
-                                               "magnetic_vector_potential");
+  hephaestus::MagnetostaticFormulation * problem_builder = new hephaestus::MagnetostaticFormulation(
+      "magnetic_reluctivity", "magnetic_permeability", "magnetic_vector_potential");
   // Set Mesh
   problem_builder->SetMesh(pmesh);
   problem_builder->AddFESpace(std::string("H1"), std::string("H1_3D_P1"));
   problem_builder->AddFESpace(std::string("HCurl"), std::string("ND_3D_P1"));
   problem_builder->AddFESpace(std::string("HDiv"), std::string("RT_3D_P0"));
-  problem_builder->AddGridFunction(std::string("magnetic_vector_potential"),
-                                   std::string("HCurl"));
-  problem_builder->AddGridFunction(std::string("source_current_density"),
-                                   std::string("HCurl"));
-  problem_builder->AddGridFunction(std::string("magnetic_flux_density"),
-                                   std::string("HDiv"));
+  problem_builder->AddGridFunction(std::string("magnetic_vector_potential"), std::string("HCurl"));
+  problem_builder->AddGridFunction(std::string("source_current_density"), std::string("HCurl"));
+  problem_builder->AddGridFunction(std::string("magnetic_flux_density"), std::string("HDiv"));
   problem_builder->registerMagneticFluxDensityAux("magnetic_flux_density");
   hephaestus::Coefficients coefficients = defineCoefficients(Itotal);
 
@@ -146,11 +147,10 @@ int main(int argc, char *argv[]) {
   A_DBC_bdr[0] = 1;
   A_DBC_bdr[1] = 2;
   A_DBC_bdr[2] = 4;
-  hephaestus::VectorDirichletBC A_DBC("magnetic_vector_potential", A_DBC_bdr,
-                                      new mfem::VectorFunctionCoefficient(3, zeroVec));
+  hephaestus::VectorDirichletBC A_DBC(
+      "magnetic_vector_potential", A_DBC_bdr, new mfem::VectorFunctionCoefficient(3, zeroVec));
 
-  problem_builder->AddBoundaryCondition("A_DBC", &A_DBC,
-                                        true);
+  problem_builder->AddBoundaryCondition("A_DBC", &A_DBC, true);
 
   problem_builder->SetCoefficients(coefficients);
 
@@ -169,14 +169,12 @@ int main(int argc, char *argv[]) {
 
   hephaestus::ProblemBuildSequencer sequencer(problem_builder);
   sequencer.ConstructEquationSystemProblem();
-  std::unique_ptr<hephaestus::SteadyStateProblem> problem =
-      problem_builder->ReturnProblem();
+  std::unique_ptr<hephaestus::SteadyStateProblem> problem = problem_builder->ReturnProblem();
   hephaestus::InputParameters exec_params;
   exec_params.SetParam("VisualisationSteps", int(1));
   exec_params.SetParam("UseGLVis", true);
   exec_params.SetParam("Problem", problem.get());
-  hephaestus::SteadyExecutioner *executioner =
-      new hephaestus::SteadyExecutioner(exec_params);
+  hephaestus::SteadyExecutioner * executioner = new hephaestus::SteadyExecutioner(exec_params);
 
   mfem::out << "Created executioner";
   executioner->Execute();
