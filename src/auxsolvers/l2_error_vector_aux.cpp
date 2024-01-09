04fb447e14f8e15311a2a30b0aab3e33afb4217f
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 16:52:42 +0000

    Updated l2_error_vector_aux and scaled_curl_vector_gridfunction_aux.

--- src/auxsolvers/l2_error_vector_aux.cpp
+++ src/auxsolvers/l2_error_vector_aux.cpp
@@ -1,20 +1,26 @@
 #include "l2_error_vector_aux.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
-L2ErrorVectorPostprocessor::L2ErrorVectorPostprocessor(
-    const hephaestus::InputParameters &params)
-    : AuxSolver(), var_name(params.GetParam<std::string>("VariableName")),
-      vec_coef_name(params.GetParam<std::string>("VectorCoefficientName")) {}
+L2ErrorVectorPostprocessor::L2ErrorVectorPostprocessor(const hephaestus::InputParameters & params)
+  : AuxSolver(),
+    var_name(params.GetParam<std::string>("VariableName")),
+    vec_coef_name(params.GetParam<std::string>("VectorCoefficientName"))
+{
+}
 
-void L2ErrorVectorPostprocessor::Init(
-    const hephaestus::GridFunctions &gridfunctions,
-    hephaestus::Coefficients &coefficients) {
+void
+L2ErrorVectorPostprocessor::Init(const hephaestus::GridFunctions & gridfunctions,
+                                 hephaestus::Coefficients & coefficients)
+{
   gf = gridfunctions.Get(var_name);
   vec_coeff = coefficients.vectors.Get(vec_coef_name);
 }
 
-void L2ErrorVectorPostprocessor::Solve(double t) {
+void
+L2ErrorVectorPostprocessor::Solve(double t)
+{
   double l2_err = gf->ComputeL2Error(*vec_coeff);
   HYPRE_BigInt ndof = gf->ParFESpace()->GlobalTrueVSize();
 
--- src/auxsolvers/scaled_curl_vector_gridfunction_aux.hpp
+++ src/auxsolvers/scaled_curl_vector_gridfunction_aux.hpp
@@ -1,18 +1,22 @@
 #pragma once
 #include "scaled_vector_gridfunction_aux.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
 // Scale the curl of a gridfunction in H(Curl) by a scalar Coefficient, and
 // store the result. Suitable for solving for H(Div) or H(Curl) conforming
 // fields for expressions like E = ρ∇×H
-class ScaledCurlVectorGridFunctionAux : public ScaledVectorGridFunctionAux {
+class ScaledCurlVectorGridFunctionAux : public ScaledVectorGridFunctionAux
+{
 public:
   ScaledCurlVectorGridFunctionAux(
-      const std::string &input_gf_name, const std::string &scaled_gf_name,
-      const std::string &coef_name, const double &aConst = 1.0,
-      const hephaestus::InputParameters &solver_options =
-          hephaestus::InputParameters());
-  virtual void buildMixedBilinearForm() override;
+      const std::string & input_gf_name,
+      const std::string & scaled_gf_name,
+      const std::string & coef_name,
+      const double & aConst = 1.0,
+      const hephaestus::InputParameters & solver_options = hephaestus::InputParameters());
+
+  void buildMixedBilinearForm() override;
 };
 } // namespace hephaestus
