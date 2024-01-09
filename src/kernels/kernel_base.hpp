535b506bb8e03eba24c8953c6ee3a30c7792ed77
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-09 17:08:22 +0000

    Updated kernels formatting.

--- src/kernels/kernel_base.hpp
+++ src/kernels/kernel_base.hpp
@@ -4,18 +4,21 @@
 #include "hephaestus_solvers.hpp"
 #include "inputs.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
-template <typename T> class Kernel {
+template <typename T>
+class Kernel
+{
 public:
   Kernel() {}
-  Kernel(const hephaestus::InputParameters &params){};
-  virtual void Init(hephaestus::GridFunctions &gridfunctions,
-                    const hephaestus::FESpaces &fespaces,
-                    hephaestus::BCMap &bc_map,
-                    hephaestus::Coefficients &coefficients){};
+  Kernel(const hephaestus::InputParameters & params){};
+  virtual void Init(hephaestus::GridFunctions & gridfunctions,
+                    const hephaestus::FESpaces & fespaces,
+                    hephaestus::BCMap & bc_map,
+                    hephaestus::Coefficients & coefficients){};
 
-  virtual void Apply(T *form) = 0;
+  virtual void Apply(T * form) = 0;
 };
 
 }; // namespace hephaestus
--- src/kernels/mixed_vector_gradient_kernel.hpp
+++ src/kernels/mixed_vector_gradient_kernel.hpp
@@ -1,21 +1,23 @@
 #pragma once
 #include "kernel_base.hpp"
 
-namespace hephaestus {
+namespace hephaestus
+{
 
 /*
 (σ ∇ V, u')
 */
-class MixedVectorGradientKernel : public Kernel<mfem::ParMixedBilinearForm> {
+class MixedVectorGradientKernel : public Kernel<mfem::ParMixedBilinearForm>
+{
 public:
-  MixedVectorGradientKernel(const hephaestus::InputParameters &params);
-  virtual void Init(hephaestus::GridFunctions &gridfunctions,
-                    const hephaestus::FESpaces &fespaces,
-                    hephaestus::BCMap &bc_map,
-                    hephaestus::Coefficients &coefficients) override;
-  virtual void Apply(mfem::ParMixedBilinearForm *mblf) override;
+  MixedVectorGradientKernel(const hephaestus::InputParameters & params);
+  virtual void Init(hephaestus::GridFunctions & gridfunctions,
+                    const hephaestus::FESpaces & fespaces,
+                    hephaestus::BCMap & bc_map,
+                    hephaestus::Coefficients & coefficients) override;
+  virtual void Apply(mfem::ParMixedBilinearForm * mblf) override;
   std::string coef_name;
-  mfem::Coefficient *coef;
+  mfem::Coefficient * coef;
 };
 
 }; // namespace hephaestus
