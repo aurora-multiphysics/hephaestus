79a322607a53dcf74d133b9b352bc347f02a9fed
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-25 13:45:57 +0000

    Factory class now returns shared pointers.

--- src/factory/factory.cpp
+++ src/factory/factory.cpp
@@ -3,69 +3,69 @@
 namespace hephaestus
 {
 
-hephaestus::ProblemBuilder *
+std::shared_ptr<hephaestus::ProblemBuilder>
 Factory::CreateProblemBuilder(std::string & formulation_name)
 {
   if (formulation_name == "EBForm")
   {
-    return new hephaestus::EBDualFormulation("magnetic_reluctivity",
-                                             "magnetic_permeability",
-                                             "electrical_conductivity",
-                                             "electric_field",
-                                             "magnetic_flux_density");
+    return std::make_shared<hephaestus::EBDualFormulation>("magnetic_reluctivity",
+                                                           "magnetic_permeability",
+                                                           "electrical_conductivity",
+                                                           "electric_field",
+                                                           "magnetic_flux_density");
   }
   else if (formulation_name == "HForm")
   {
-    return new hephaestus::HFormulation("electrical_resistivity",
-                                        "electrical_conductivity",
-                                        "magnetic_permeability",
-                                        "magnetic_field");
+    return std::make_shared<hephaestus::HFormulation>("electrical_resistivity",
+                                                      "electrical_conductivity",
+                                                      "magnetic_permeability",
+                                                      "magnetic_field");
   }
   else if (formulation_name == "AForm")
   {
-    return new hephaestus::AFormulation("magnetic_reluctivity",
-                                        "magnetic_permeability",
-                                        "electrical_conductivity",
-                                        "magnetic_vector_potential");
+    return std::make_shared<hephaestus::AFormulation>("magnetic_reluctivity",
+                                                      "magnetic_permeability",
+                                                      "electrical_conductivity",
+                                                      "magnetic_vector_potential");
   }
   else if (formulation_name == "EForm")
   {
-    return new hephaestus::EFormulation("magnetic_reluctivity",
-                                        "magnetic_permeability",
-                                        "electrical_conductivity",
-                                        "electric_field");
+    return std::make_shared<hephaestus::EFormulation>("magnetic_reluctivity",
+                                                      "magnetic_permeability",
+                                                      "electrical_conductivity",
+                                                      "electric_field");
   }
   else if (formulation_name == "AVForm")
   {
-    return new hephaestus::AVFormulation("magnetic_reluctivity",
-                                         "magnetic_permeability",
-                                         "electrical_conductivity",
-                                         "magnetic_vector_potential",
-                                         "electric_potential");
+    return std::make_shared<hephaestus::AVFormulation>("magnetic_reluctivity",
+                                                       "magnetic_permeability",
+                                                       "electrical_conductivity",
+                                                       "magnetic_vector_potential",
+                                                       "electric_potential");
   }
   else if (formulation_name == "ComplexEForm")
   {
-    return new hephaestus::ComplexEFormulation("magnetic_reluctivity",
-                                               "electrical_conductivity",
-                                               "dielectric_permittivity",
-                                               "frequency",
-                                               "electric_field",
-                                               "electric_field_real",
-                                               "electric_field_imag");
+    return std::make_shared<hephaestus::ComplexEFormulation>("magnetic_reluctivity",
+                                                             "electrical_conductivity",
+                                                             "dielectric_permittivity",
+                                                             "frequency",
+                                                             "electric_field",
+                                                             "electric_field_real",
+                                                             "electric_field_imag");
   }
   else if (formulation_name == "ComplexAForm")
   {
-    return new hephaestus::ComplexAFormulation("magnetic_reluctivity",
-                                               "electrical_conductivity",
-                                               "dielectric_permittivity",
-                                               "frequency",
-                                               "magnetic_vector_potential",
-                                               "magnetic_vector_potential_real",
-                                               "magnetic_vector_potential_imag");
+    return std::make_shared<hephaestus::ComplexAFormulation>("magnetic_reluctivity",
+                                                             "electrical_conductivity",
+                                                             "dielectric_permittivity",
+                                                             "frequency",
+                                                             "magnetic_vector_potential",
+                                                             "magnetic_vector_potential_real",
+                                                             "magnetic_vector_potential_imag");
   }
   else if (formulation_name == "Custom")
   {
-    return new hephaestus::TimeDomainEMFormulation();
+    return std::make_shared<hephaestus::TimeDomainEMFormulation>();
   }
   else
   {
@@ -74,28 +74,28 @@ Factory::CreateProblemBuilder(std::string & formulation_name)
   }
 };
 
-hephaestus::FrequencyDomainEMFormulation *
+std::shared_ptr<hephaestus::FrequencyDomainEMFormulation>
 Factory::CreateFrequencyDomainEmFormulation(std::string & formulation)
 {
   if (formulation == "ComplexEForm")
   {
-    return new hephaestus::ComplexEFormulation("magnetic_reluctivity",
-                                               "electrical_conductivity",
-                                               "dielectric_permittivity",
-                                               "frequency",
-                                               "electric_field",
-                                               "electric_field_real",
-                                               "electric_field_imag");
+    return std::make_shared<hephaestus::ComplexEFormulation>("magnetic_reluctivity",
+                                                             "electrical_conductivity",
+                                                             "dielectric_permittivity",
+                                                             "frequency",
+                                                             "electric_field",
+                                                             "electric_field_real",
+                                                             "electric_field_imag");
   }
   else if (formulation == "ComplexAForm")
   {
-    return new hephaestus::ComplexAFormulation("magnetic_reluctivity",
-                                               "electrical_conductivity",
-                                               "dielectric_permittivity",
-                                               "frequency",
-                                               "magnetic_vector_potential",
-                                               "magnetic_vector_potential_real",
-                                               "magnetic_vector_potential_imag");
+    return std::make_shared<hephaestus::ComplexAFormulation>("magnetic_reluctivity",
+                                                             "electrical_conductivity",
+                                                             "dielectric_permittivity",
+                                                             "frequency",
+                                                             "magnetic_vector_potential",
+                                                             "magnetic_vector_potential_real",
+                                                             "magnetic_vector_potential_imag");
   }
   else
   {
@@ -104,49 +104,49 @@ Factory::CreateFrequencyDomainEmFormulation(std::string & formulation)
   return nullptr;
 }
 
-hephaestus::TimeDomainEMFormulation *
+std::shared_ptr<hephaestus::TimeDomainEMFormulation>
 Factory::CreateTimeDomainEmFormulation(std::string & formulation)
 {
   if (formulation == "EBForm")
   {
-    return new hephaestus::EBDualFormulation("magnetic_reluctivity",
-                                             "magnetic_permeability",
-                                             "electrical_conductivity",
-                                             "electric_field",
-                                             "magnetic_flux_density");
+    return std::make_shared<hephaestus::EBDualFormulation>("magnetic_reluctivity",
+                                                           "magnetic_permeability",
+                                                           "electrical_conductivity",
+                                                           "electric_field",
+                                                           "magnetic_flux_density");
   }
   else if (formulation == "HForm")
   {
-    return new hephaestus::HFormulation("electrical_resistivity",
-                                        "electrical_conductivity",
-                                        "magnetic_permeability",
-                                        "magnetic_field");
+    return std::make_shared<hephaestus::HFormulation>("electrical_resistivity",
+                                                      "electrical_conductivity",
+                                                      "magnetic_permeability",
+                                                      "magnetic_field");
   }
   else if (formulation == "AForm")
   {
-    return new hephaestus::AFormulation("magnetic_reluctivity",
-                                        "magnetic_permeability",
-                                        "electrical_conductivity",
-                                        "magnetic_vector_potential");
+    return std::make_shared<hephaestus::AFormulation>("magnetic_reluctivity",
+                                                      "magnetic_permeability",
+                                                      "electrical_conductivity",
+                                                      "magnetic_vector_potential");
   }
   else if (formulation == "EForm")
   {
-    return new hephaestus::EFormulation("magnetic_reluctivity",
-                                        "magnetic_permeability",
-                                        "electrical_conductivity",
-                                        "electric_field");
+    return std::make_shared<hephaestus::EFormulation>("magnetic_reluctivity",
+                                                      "magnetic_permeability",
+                                                      "electrical_conductivity",
+                                                      "electric_field");
   }
   else if (formulation == "AVForm")
   {
-    return new hephaestus::AVFormulation("magnetic_reluctivity",
-                                         "magnetic_permeability",
-                                         "electrical_conductivity",
-                                         "magnetic_vector_potential",
-                                         "electric_potential");
+    return std::make_shared<hephaestus::AVFormulation>("magnetic_reluctivity",
+                                                       "magnetic_permeability",
+                                                       "electrical_conductivity",
+                                                       "magnetic_vector_potential",
+                                                       "electric_potential");
   }
   else if (formulation == "Custom")
   {
-    return new hephaestus::TimeDomainEMFormulation();
+    return std::make_shared<hephaestus::TimeDomainEMFormulation>();
   }
   else
   {
@@ -155,7 +155,7 @@ Factory::CreateTimeDomainEmFormulation(std::string & formulation)
   return nullptr;
 }
 
-mfem::ParFiniteElementSpace *
+std::shared_ptr<mfem::ParFiniteElementSpace>
 Factory::CreateParFESpace(hephaestus::InputParameters params, mfem::ParMesh & pmesh)
 {
   auto fe_type(params.GetParam<std::string>("FESpaceType"));
@@ -164,19 +164,19 @@ Factory::CreateParFESpace(hephaestus::InputParameters params, mfem::ParMesh & pm
                                                       // FiniteElementCollection::New instead
   if (fe_type == "H1")
   {
-    return new mfem::common::H1_ParFESpace(&pmesh, order, components);
+    return std::make_shared<mfem::common::H1_ParFESpace>(&pmesh, order, components);
   }
   else if (fe_type == "ND")
   {
-    return new mfem::common::ND_ParFESpace(&pmesh, order, components);
+    return std::make_shared<mfem::common::ND_ParFESpace>(&pmesh, order, components);
   }
   else if (fe_type == "RT")
   {
-    return new mfem::common::RT_ParFESpace(&pmesh, order, components);
+    return std::make_shared<mfem::common::RT_ParFESpace>(&pmesh, order, components);
   }
   else if (fe_type == "L2")
   {
-    return new mfem::common::L2_ParFESpace(&pmesh, order, components);
+    return std::make_shared<mfem::common::L2_ParFESpace>(&pmesh, order, components);
   }
   else
   {
