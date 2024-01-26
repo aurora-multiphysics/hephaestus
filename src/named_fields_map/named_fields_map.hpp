76a051d254a2d53331079d93f8ccb01ee1e0d57d
Edward Palmer <edward.palmer@ukaea.uk>
2024-01-26 13:14:59 +0000

    Simplified NamedFieldsMap to only store shared pointers.

--- src/named_fields_map/named_fields_map.hpp
+++ src/named_fields_map/named_fields_map.hpp
@@ -14,7 +14,7 @@ template <typename T>
 class NamedFieldsMap
 {
 public:
-  using MapType = std::map<std::string, T *>;
+  using MapType = std::map<std::string, std::shared_ptr<T>>;
   using iterator = typename MapType::iterator;
   using const_iterator = typename MapType::const_iterator;
 
@@ -24,50 +24,25 @@ public:
   /// Destructor.
   ~NamedFieldsMap() { DeregisterAll(); }
 
-  /// Construct new field with name @field_name and register (own_data = true).
+  /// Construct new field with name @field_name and register.
   template <class FieldType, class... FieldArgs>
   void Register(const std::string & field_name, FieldArgs &&... args)
   {
     Register(field_name, std::make_shared<FieldType>(std::forward<FieldArgs>(args)...));
   }
 
-  /// Register association between field @a field and name @a field_name
-  // void Register(const std::string & field_name, T * field, bool own_data)
-  // {
-  //   // 0. Prevent double-registration of field.
-  //   CheckForDoubleRegistration(field_name, field);
-
-  //   // 1. Deregister existing field with that name.
-  //   Deregister(field_name);
-
-  //   // 2. Add to field map.
-  //   _field_map[field_name] = field;
-
-  //   // 3. Keep track of ownership.
-  //   if (own_data)
-  //   {
-  //     _stored_ptrs_map.emplace(field_name, std::shared_ptr<T>(field));
-  //   }
-  // }
-
-  /// Register with a shared pointer (own_data = true).
+  /// Register association between field @a field and name @a field_name.
   void Register(const std::string & field_name, std::shared_ptr<T> field)
   {
     CheckForDoubleRegistration(field_name, field.get());
 
     Deregister(field_name);
 
-    _field_map[field_name] = field.get();
-
-    _stored_ptrs_map[field_name] = std::move(field);
+    _field_map[field_name] = std::move(field);
   }
 
   /// Unregister association between field @a field and name @a field_name.
-  void Deregister(const std::string & field_name)
-  {
-    _field_map.erase(field_name);
-    _stored_ptrs_map.erase(field_name);
-  }
+  void Deregister(const std::string & field_name) { _field_map.erase(field_name); }
 
   /// Predicate to check if a field is associated with name @a field_name.
   [[nodiscard]] inline bool Has(const std::string & field_name) const
@@ -75,27 +50,11 @@ public:
     return find(field_name) != end();
   }
 
-  /// Get a pointer to the field associated with name @a field_name.
-  [[nodiscard]] inline T * Get(const std::string & field_name) const
-  {
-    auto it = find(field_name);
-    return it != _field_map.end() ? it->second : nullptr;
-  }
-
-  /// Get a pointer to the field and cast to subclass TDerived.
-  template <typename TDerived>
-  [[nodiscard]] inline TDerived * Get(const std::string & field_name) const
-  {
-    auto ptr = Get(field_name);
-
-    return ptr ? std::dynamic_pointer_cast<TDerived>(ptr) : nullptr;
-  }
-
   /// Get a shared pointer to the field associated with name @a field_name.
   [[nodiscard]] inline std::shared_ptr<T> GetShared(const std::string & field_name) const
   {
-    auto it = _stored_ptrs_map.find(field_name);
-    return it != _stored_ptrs_map.end() ? it->second : nullptr;
+    auto it = find(field_name);
+    return it != _field_map.end() ? it->second : nullptr;
   }
 
   /// Get a shared pointer to the field and cast to subclass TDerived.
@@ -105,6 +64,23 @@ public:
     auto shared_ptr = GetShared(field_name);
 
     return shared_ptr ? std::dynamic_pointer_cast<TDerived>(shared_ptr) : nullptr;
+  }
+
+  /// Get a pointer to the field associated with name @a field_name.
+  [[nodiscard]] inline T * Get(const std::string & field_name) const
+  {
+    auto shared_ptr = GetShared(field_name);
+
+    return shared_ptr ? shared_ptr.get() : nullptr;
+  }
+
+  /// Get a pointer to the field and cast to subclass TDerived.
+  template <typename TDerived>
+  [[nodiscard]] inline TDerived * Get(const std::string & field_name) const
+  {
+    auto shared_ptr = GetShared<TDerived>(field_name);
+
+    return shared_ptr ? shared_ptr.get() : nullptr;
   }
 
   /// Returns a vector containing all values for supplied keys.
@@ -175,14 +151,9 @@ protected:
   }
 
   /// Clear all associations between names and fields.
-  void DeregisterAll()
-  {
-    _field_map.clear();
-    _stored_ptrs_map.clear();
-  }
+  void DeregisterAll() { _field_map.clear(); }
 
 private:
   MapType _field_map{};
-  std::map<std::string, std::shared_ptr<T>> _stored_ptrs_map{};
 };
 } // namespace hephaestus
