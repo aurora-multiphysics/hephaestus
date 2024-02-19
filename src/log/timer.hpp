1c86db31b3ac7b7d6bca03b99cedc22d0c8def1a
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2024-02-19 17:00:15 +0000

    Overloaded timer template for different measurements

--- src/log/timer.hpp
+++ src/log/timer.hpp
@@ -1,35 +1,53 @@
 #pragma once
 
-#include <ostream>
 #include <chrono>
+#include <ostream>
+#include <iomanip>
 
 namespace hephaestus
 {
 
+template <typename T = std::chrono::microseconds>
 class Timer
 {
   using Clock = std::chrono::high_resolution_clock;
   using TimePoint = std::chrono::time_point<Clock>;
 
+private:
   TimePoint _timerstart;
   int _precision = 6;
 
+public:
   Timer() : _timerstart(Clock::now()) {}
   Timer(int precision) : _timerstart(Clock::now()), _precision(precision) {}
 
-  [[nodiscard]] inline double Seconds() const
+  [[nodiscard]] inline double Microsec() const
   {
-    auto sec = std::chrono::duration_cast<std::chrono::seconds>(Clock::now() - _timerstart);
-    return sec.count();
+    auto usec = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now() - _timerstart);
+    return usec.count();
   }
 
   friend std::ostream & operator<<(std::ostream & stream, const Timer & timer);
 };
 
 inline std::ostream &
-operator<<(std::ostream & stream, const Timer & timer)
+operator<<(std::ostream & stream, const Timer<std::chrono::microseconds> & timer)
 {
-  stream << timer.Seconds() << " s";
+  stream << std::fixed << std::setprecision(timer._precision) << timer.Microsec() << " μs";
+  return stream;
+}
+
+inline std::ostream &
+operator<<(std::ostream & stream, const Timer<std::chrono::milliseconds> & timer)
+{
+  stream << std::fixed << std::setprecision(timer._precision) << timer.Microsec() / 1e3 << " ms";
+  return stream;
+}
+
+inline std::ostream &
+operator<<(std::ostream & stream, const Timer<std::chrono::seconds> & timer)
+{
+  stream << std::fixed << std::setprecision(timer._precision) << timer.Microsec() / 1e6 << " s";
   return stream;
 }
 
