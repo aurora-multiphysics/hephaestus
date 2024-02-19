e36b52d485cfd0f27c8f75a26395155867921a6a
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2024-02-19 15:30:02 +0000

    Added logger to hephaestus.h and created base functions for timer

--- src/hephaestus.hpp
+++ src/hephaestus.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "../common/pfem_extras.hpp"
 #include "factory.hpp"
+#include "logger.hpp"
 #include "inputs.hpp"
 #include "problem_builder.hpp"
 #include "steady_executioner.hpp"
--- src/log/timer.hpp
+++ src/log/timer.hpp
@@ -8,6 +8,18 @@ namespace hephaestus
 
 class Timer
 {
+  using Clock = std::chrono::high_resolution_clock;
+  using TimePoint = std::chrono::time_point<Clock>;
+
+  TimePoint _timerstart;
+
+  Timer() : _timerstart(Clock::now()) {}
+
+  inline double Microsec()
+  {
+    auto musec = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now() - _timerstart);
+    return musec.count();
+  }
 };
 
 } // namespace hephaestus
