174f03fe17c94d9a9d41b345b6f9ae4724f10812
Edward Palmer <edward.palmer@ukaea.uk>
2024-02-23 14:19:54 +0000

    Split SteadyStateProblem out into a version with an equation system and another without an equation system.

--- src/problem_builders/problem_builder.hpp
+++ src/problem_builders/problem_builder.hpp
@@ -2,4 +2,5 @@
 #include "logging.hpp"
 #include "factory.hpp"
 #include "steady_state_problem_builder.hpp"
+#include "steady_state_equation_system_problem_builder.hpp"
 #include "time_domain_problem_builder.hpp"
---
+++ src/problem_builders/steady_state_equation_system_problem_builder.hpp
@@ -0,0 +1,84 @@
+#pragma once
+#include "problem_operator.hpp"
+#include "problem_builder_base.hpp"
+namespace hephaestus
+{
+
+/// Class for steady-state problems with an equation system.
+class SteadyStateEquationSystemProblem : public hephaestus::Problem
+{
+public:
+  friend class SteadyStateEquationSystemProblemBuilder;
+
+  SteadyStateEquationSystemProblem() = default;
+  ~SteadyStateEquationSystemProblem() override = default;
+
+  [[nodiscard]] hephaestus::EquationSystem * GetEquationSystem() const override
+  {
+    return GetOperator()->GetEquationSystem();
+  }
+
+  [[nodiscard]] hephaestus::EquationSystemProblemOperator * GetOperator() const override
+  {
+    if (!_ss_operator)
+    {
+      MFEM_ABORT("No ProblemOperator has been added to SteadyStateProblem.");
+    }
+
+    return _ss_operator.get();
+  }
+
+  void SetOperator(std::unique_ptr<EquationSystemProblemOperator> new_problem_operator)
+  {
+    _ss_operator.reset();
+    _ss_operator = std::move(new_problem_operator);
+  }
+
+private:
+  std::unique_ptr<hephaestus::EquationSystemProblemOperator> _ss_operator{nullptr};
+};
+
+// Builder class of a frequency-domain problem.
+class SteadyStateEquationSystemProblemBuilder : public hephaestus::ProblemBuilder
+{
+public:
+  SteadyStateEquationSystemProblemBuilder()
+    : _problem(std::make_unique<hephaestus::SteadyStateEquationSystemProblem>())
+  {
+  }
+
+  ~SteadyStateEquationSystemProblemBuilder() override = default;
+
+  virtual std::unique_ptr<hephaestus::SteadyStateEquationSystemProblem> ReturnProblem()
+  {
+    return std::move(_problem);
+  }
+
+  void RegisterFESpaces() override {}
+
+  void RegisterGridFunctions() override {}
+
+  void RegisterAuxSolvers() override {}
+
+  void RegisterCoefficients() override {}
+
+  void InitializeKernels() override;
+
+  void ConstructEquationSystem() override;
+
+  void SetOperatorGridFunctions() override;
+
+  void ConstructOperator() override;
+
+  void ConstructState() override;
+
+  void ConstructTimestepper() override {}
+
+protected:
+  std::unique_ptr<hephaestus::SteadyStateEquationSystemProblem> _problem{nullptr};
+  mfem::ConstantCoefficient _one_coef{1.0};
+
+  hephaestus::SteadyStateEquationSystemProblem * GetProblem() override { return _problem.get(); };
+};
+
+} // namespace hephaestus
