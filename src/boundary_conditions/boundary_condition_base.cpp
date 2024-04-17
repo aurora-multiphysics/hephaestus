fe9a4ab5b93ae8d2c46b263b2d21d05e8b23d864
Edward Palmer <edward.palmer@ukaea.uk>
2024-04-17 12:15:34 +0000

    Boundary conditions now have Update method to update their boundary attributes array on mesh refinement.

--- src/boundary_conditions/boundary_condition_base.cpp
+++ src/boundary_conditions/boundary_condition_base.cpp
@@ -17,4 +17,44 @@ BoundaryCondition::GetMarkers(mfem::Mesh & mesh)
   return _markers;
 }
 
+void
+BoundaryCondition::Update(mfem::Mesh & mesh)
+{
+  // If the mesh has been refined, the number of boundary attributes will have
+  // changed! We will need to map from our old bdr_attributes array to the new
+  // one.
+  if (mesh.bdr_attributes.Size() == _bdr_attributes.Size())
+  {
+    return;
+  }
+
+  // Ensure that we have non-empty arrays.
+  if (mesh.bdr_attributes.Size() == 0 || _bdr_attributes.Size() == 0)
+  {
+    MFEM_ABORT("Encountered empty boundary attribute arrays.");
+  }
+
+  // NB: assume x2 for 2D uniform refinement; x4 for 3D.
+  int refinement_factor = (mesh.bdr_attributes.Size() / _bdr_attributes.Size());
+
+  if (refinement_factor != 2 && refinement_factor != 4)
+  {
+    MFEM_ABORT("Cannot handle refinement factor: " << refinement_factor);
+  }
+
+  mfem::Array<int> new_bdr_attributes(mesh.bdr_attributes.Size());
+
+  for (int i = 0; i < _bdr_attributes.Size(); i++)
+  {
+    int attr = _bdr_attributes[i];
+
+    for (int k = 0; k < refinement_factor; k++)
+    {
+      new_bdr_attributes[i * refinement_factor + k] = attr;
+    }
+  }
+
+  _bdr_attributes = new_bdr_attributes;
+}
+
 } // namespace hephaestus
