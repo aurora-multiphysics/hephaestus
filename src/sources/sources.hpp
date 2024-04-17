e147aac043e4e28033e08695e2169124b4238f2c
Edward Palmer <edward.palmer@ukaea.uk>
2024-04-17 12:30:40 +0000

    Added Update method for Sources.

--- src/sources/sources.hpp
+++ src/sources/sources.hpp
@@ -15,8 +15,15 @@ public:
             const hephaestus::FESpaces & fespaces,
             hephaestus::BCMap & bc_map,
             hephaestus::Coefficients & coefficients);
+
   void Apply(mfem::ParLinearForm * lf);
   void SubtractSources(mfem::ParGridFunction * gf);
+
+  /// @brief Called on mesh refinement.
+  void Update(hephaestus::GridFunctions & gridfunctions,
+              const hephaestus::FESpaces & fespaces,
+              hephaestus::BCMap & bc_map,
+              hephaestus::Coefficients & coefficients);
 };
 
 } // namespace hephaestus
