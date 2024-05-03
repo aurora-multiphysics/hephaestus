196f1d39fd736fbf67e2489b78665d883d8f2483
Edward Palmer <edward.palmer@ukaea.uk>
2024-05-03 10:50:54 +0000

    Added a JacobianOperatorHandle accessor method to EquationSystem. Currently commented-out.

--- src/equation_systems/equation_system.hpp
+++ src/equation_systems/equation_system.hpp
@@ -97,6 +97,9 @@ public:
   virtual void RecoverFEMSolution(mfem::BlockVector & trueX,
                                   hephaestus::GridFunctions & gridfunctions);
 
+  /// Returns a reference to the jacobian operator handle.
+  //[[nodiscard]] mfem::OperatorHandle & JacobianOperatorHandle() { return _jacobian; }
+
   std::vector<mfem::Array<int>> _ess_tdof_lists;
 
 protected:
