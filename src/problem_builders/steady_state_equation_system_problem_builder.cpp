0239d6488cd1dee1b336626a3e704dd9c257f90c
Edward Palmer <edward.palmer@ukaea.uk>
2024-05-16 12:47:23 +0000

    Removed empty files.

--- src/problem_builders/steady_state_equation_system_problem_builder.cpp
+++
@@ -1,6 +0,0 @@
-#include "steady_state_equation_system_problem_builder.hpp"
-
-namespace hephaestus
-{
-
-} // namespace hephaestus
--- src/problem_builders/time_domain_equation_system_problem_builder.cpp
+++
@@ -1,6 +0,0 @@
-#include "time_domain_equation_system_problem_builder.hpp"
-
-namespace hephaestus
-{
-
-} // namespace hephaestus
