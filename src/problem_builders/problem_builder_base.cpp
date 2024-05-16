2e4823e26ca7b6ff896b58f76971f72981be7b08
Edward Palmer <edward.palmer@ukaea.uk>
2024-05-16 12:47:23 +0000

    ProblemBuilder now implements ConstructState method.

--- src/problem_builders/problem_builder_base.cpp
+++ src/problem_builders/problem_builder_base.cpp
@@ -377,6 +377,16 @@ ProblemBuilder::InitializeOutputs()
 }
 
 void
+ProblemBuilder::ConstructState()
+{
+  auto problem_operator = GetProblem()->GetOperator();
+
+  GetProblem()->_f =
+      std::make_unique<mfem::BlockVector>(problem_operator->_true_offsets); // Vector of dofs
+  problem_operator->Init(*(GetProblem()->_f)); // Set up initial conditions
+}
+
+void
 ProblemBuilder::InitializeOperator()
 {
   InitializeEquationSystem();
