59d8dc1cd8f38f5d665e11ddd95b5a5189ed704a
Edward Palmer <edward.palmer@ukaea.uk>
2024-05-16 12:47:23 +0000

    Added Update method for auxsolvers.

--- src/auxsolvers/auxsolvers.hpp
+++ src/auxsolvers/auxsolvers.hpp
@@ -24,7 +24,12 @@ public:
   std::vector<std::pair<std::shared_ptr<hephaestus::AuxSolver>, std::string>> _aux_queue;
   void Init(const hephaestus::GridFunctions & gridfunctions,
             hephaestus::Coefficients & coefficients);
+
   void Solve(double t = 0.0);
+
+  /// @brief Update method called on mesh refinement. Calls Init.
+  virtual void Update(const hephaestus::GridFunctions & gridfunctions,
+                      hephaestus::Coefficients & coefficients);
 };
 
 } // namespace hephaestus
