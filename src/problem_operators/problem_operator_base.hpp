3562141911f7ba95626e96b3b25f1e439e3c810c
Edward Palmer <edward.palmer@ukaea.uk>
2024-05-20 14:10:19 +0000

    Added comments to ProblemOperatorBase methods.

--- src/problem_operators/problem_operator_base.hpp
+++ src/problem_operators/problem_operator_base.hpp
@@ -10,23 +10,35 @@ class ProblemOperatorBase
 {
 public:
   ProblemOperatorBase(hephaestus::Problem & problem) : _problem(problem) {}
+
   virtual ~ProblemOperatorBase() = default;
 
+  /// Initialize the problem operator.
   virtual void Init();
 
+  /// Update the problem operator after a mesh change.
   virtual void Update();
 
+  /// Arrays of offsets.
   mfem::Array<int> _true_offsets, _block_true_offsets;
 
+  /// Block vectors.
   mfem::BlockVector _true_x, _true_rhs;
 
 protected:
+  /// Set trial variables names. Override in derived classes.
   virtual void SetTrialVariableNames() {}
+
+  /// Set gridfunction of trial variables from the trial variable names.
   virtual void SetTrialVariables();
+
+  /// Update the block vectors and offsets after a mesh change.
   virtual void UpdateOffsets();
 
+  /// Update a block vector. Should be called after the offsets have been updated.
   virtual void UpdateBlockVector(mfem::BlockVector & X);
 
+  /// Called by UpdateOffsets.
   virtual void UpdateOffsetsWithSize(size_t soln_vector_size);
 
   /// Returns a reference to the operator's width.
@@ -38,9 +50,11 @@ protected:
   // Reference to the current problem.
   hephaestus::Problem & _problem;
 
-  // Vector of names of state gridfunctions used in formulation, ordered by appearance in block
-  // vector during solve.
+  /// Vector of names of state gridfunctions used in formulation, ordered by
+  /// appearance in block vector during solve.
   std::vector<std::string> _trial_var_names;
+
+  /// Vector of trial variables set by SetTrialVariables.
   std::vector<mfem::ParGridFunction *> _trial_variables;
 };
 }
