40e8727a3b7f5417dfc0d0a72ced5641802f04d2
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-03 15:37:29 +0000

    Moves protected section beneath public to be consistent; adds new-lines to space-out code.

--- src/io/inputs.hpp
+++ src/io/inputs.hpp
@@ -14,18 +14,18 @@ namespace hephaestus
 
 class InputParameters
 {
-
-protected:
-  std::map<std::string, std::any> _params;
-
 public:
   InputParameters() = default;
+
   InputParameters(std::map<std::string, std::any> _params) : _params(std::move(_params)) {}
+
   void SetParam(std::string param_name, std::any value) { _params[param_name] = value; }
+
   template <typename T>
   [[nodiscard]] T GetParam(std::string param_name) const
   {
     T param;
+
     try
     {
       param = std::any_cast<T>(_params.at(param_name));
@@ -37,10 +37,12 @@ public:
     }
     return param;
   }
+
   template <typename T>
   [[nodiscard]] T GetOptionalParam(std::string param_name, T value) const
   {
     T param;
+
     try
     {
       param = std::any_cast<T>(_params.at(param_name));
@@ -51,6 +53,9 @@ public:
     }
     return param;
   }
+
+protected:
+  std::map<std::string, std::any> _params;
 };
 
 } // namespace hephaestus
